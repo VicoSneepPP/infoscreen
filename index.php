e363c0593cdfe88b31761522f332457d5b0127b9
Fleaco <vico.sneep@plusport.com>
2016-05-31 17:28:20 +0200

    #15 created a dynamic page select of venues with redirect

--- index.php
+++ index.php
@@ -1,34 +1,35 @@
 <?php get_header(); ?>
 
 	<main role="main">
-
+	<div class=" col-md-offset-2 col-md-8">
+			<img src="<?php echo ot_get_option('pp_logo');?>" alt="Logo" class="logo-home">
+	</div>
 	<div class="row">
-		<div class="col-md-7">
-			<section id="hoofdberichten">
-
-				<?php get_template_part('partials/loop-main'); ?>
-
-			</section>
+		<div class=" col-md-offset-3 col-md-6">
+			<section id="cityselect">
+				<div class="col-md-12">
+					<div class="pageselect"><?php _e('Kies je vestiging', 'pplang'); ?></div>
+				</div>
+				<div class="col-md-12">
+					<form action="<?php bloginfo('url'); ?>" method="get">
+						<?php wp_dropdown_pages(); ?>
+						<input class="home" type="submit" name="submit" value="Bekijk vestiging" />
+					</form>
+				</div>
 		</div>
 
-		<div class="col-md-5">
+			</section>
+
+
+		<div class=" col-md-offset-3 col-md-6">
 			<div class="row">
 
-				<section id="weather">
+				<section id="weatherhome">
 					<?php get_template_part('partials/partial-weather'); ?>
 				</section>
 
-				<section id="kleinberichten">
-
-					<?php get_template_part('partials/loop-klein'); ?>
-
-				</section>
 
 			</div>
 		</div>
 	</div>
 	</main>
-
-<!--?php get_sidebar(); ?>
-
-<?php get_footer(); ?>
