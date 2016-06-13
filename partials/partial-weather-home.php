3ab84c5a9d2a87b243ed7b82e73b5353a946ff0e
Fleaco <vico.sneep@plusport.com>
2016-06-13 11:24:26 +0200

    #20 partial created for home page

---
+++ partials/partial-weather-home.php
@@ -0,0 +1,15 @@
+<!--WEER - DEZE WORDT IN addons/ppclock/ppclock.js INGELADEN -->
+<div class="col-md-12">
+	<div id="weer">
+		<div id="weer-background">
+			<div id="weer-bg-seperator-1" class="weer-bg-seperator"></div>
+			<div id="weer-bg-seperator-2" class="weer-bg-seperator"></div>
+			<div id="weer-bg-seperator-3" class="weer-bg-seperator"></div>
+		</div>
+		<div id="weer-tijd"></div>
+		<div id="weer-type"></div>
+		<div id="weer-icon"></div>
+		<div id="weer-date" class="home"></div>
+		<div id="weer-temp"></div>
+	</div>
+</div>
