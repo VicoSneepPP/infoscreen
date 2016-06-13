2a6800798bd3b5468c675bfba7a27b936339c581
Fleaco <vico.sneep@plusport.com>
2016-06-13 11:24:13 +0200

    #20 hide location change page

---
+++ index_multipage.php
@@ -0,0 +1,40 @@
+<?php get_header(); ?>
+
+	<main role="main">
+	<div class=" col-md-offset-2 col-md-8">
+			<img src="<?php echo ot_get_option('pp_logo');?>" alt="Logo" class="logo-home">
+	</div>
+	<div class="row">
+		<div class=" col-md-offset-3 col-md-6">
+			<section id="cityselect">
+				<div class="col-md-12">
+					<div class="pageselect"><?php _e('Kies je vestiging', 'pplang'); ?></div>
+				</div>
+				<div class="col-md-12">
+					<form action="<?php bloginfo('url'); ?>" method="get">
+						<?php
+							wp_dropdown_pages();
+						?>
+
+						<input class="home" type="submit" name="submit" value="Bekijk vestiging" />
+					</form>
+				</div>
+		</div>
+
+			</section>
+
+
+		<div class=" col-md-offset-3 col-md-6">
+			<div class="row">
+
+				<section id="weatherhome">
+					<div id="weer">
+					<div id="weer-tijd"></div>
+					</div>
+				</section>
+
+
+			</div>
+		</div>
+	</div>
+	</main>
